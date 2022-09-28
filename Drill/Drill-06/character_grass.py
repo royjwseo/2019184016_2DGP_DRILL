36df3127de19eb0a64f9b86092b45a7e3591d805
royjwseo <99865417+royjwseo@users.noreply.github.com>
2022-09-28 17:45:57 +0900

    drill07완성

--- Drill/Drill-06/character_grass.py
+++ Drill/Drill-06/character_grass.py
@@ -1,40 +1,39 @@
 from pico2d import *
 
-open_canvas()
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+def handle_events():
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSEMOTION:
+            if event.x > x:
+                ToRight = True
+            else:
+                ToRight = False
+            x, y = event.x, KPU_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+    pass
 
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
 # fill here
+kpu_ground = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
 
-open_canvas()
+running = True
+ToRight = True
+x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+frame = 0
+hide_cursor()
 
-grass = load_image('grass.png')
-character = load_image('character.png')
-import math
+while running:
+    clear_canvas()
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    if(ToRight):
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    else:
+        character.clip_draw(frame * 100, 100 * 0, 100, 100, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
 
-x = 2
-y = 90
-r = 300
-ang = 0
-a = 400
-
-count = -1
-while (count % 2 == 0 or count == -1):
-
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, y)
-
-    if x == 398 and y == 90:
-        count = count + 1
-    if y == 90:
-        x = x + 2
-    if x == 798:
-        y = y + 2
-    if y == 598:
-        x = x - 2
-    if x == 2 and y != 90:
-        y = y - 2
-
-    delay(0.01)
-
-close_canvas()
-
+    handle_events()
